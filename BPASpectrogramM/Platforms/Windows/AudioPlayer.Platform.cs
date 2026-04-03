a68e9354d570f3a679d70538197f843971299130
Justin A T halls <justin.halls@echolocation.org.uk>
2026-04-03 16:19:23 +0100

    And plays on Windows

--- BPASpectrogramM/Platforms/Windows/AudioPlayer.Platform.cs
+++ BPASpectrogramM/Platforms/Windows/AudioPlayer.Platform.cs
@@ -2,6 +2,4 @@ namespace BPASpectrogramM.Views;
 
 public partial class AudioPlayer
 {
-    private partial IAudioPlaybackService? CreatePlatformAudioPlaybackService()
-        => new BPASpectrogramM.Platforms.Windows.AudioPlaybackService();
 }
